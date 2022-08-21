31a6a373d93e6991c5b244cb4fbe9ea2f64e8644
filip <fnovakovic8820rn@raf.rs>
2022-08-21 23:47:16 +0200

    finally

--- app.js
+++ app.js
@@ -63,18 +63,18 @@ app.use('/admin', rez);
 
 
 
-app.get('/register', (req, res) => {
-    res.sendFile('register.html', { root: './static' });
-});
+// app.get('/register', (req, res) => {
+//     res.sendFile('register.html', { root: './static' });
+// });
 
-app.get('/login', (req, res) => {
-    res.sendFile('login.html', { root: './static' });
+// app.get('/login', (req, res) => {
+//     res.sendFile('login.html', { root: './static' });
     
-});
+// });
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: './static' });
-});
+// app.get('/', (req, res) => {
+//     res.sendFile('index.html', { root: './static' });
+// });
 
 
 app.use(express.static(path.join(__dirname, 'static')));
